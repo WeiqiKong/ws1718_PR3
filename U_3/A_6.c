ce28a7f590a421788eeef656715a26bb5b27d855
weiqi <kongweiqi19@gmail.com>
2017-10-15 15:38:53 +0200

    Ueb 3 Aufgabe 7 2017年10月15日 星期日

--- U_3/A_6.c
+++ U_3/A_6.c
@@ -5,15 +5,27 @@
 #include <stdio.h>
 
 int main(void) {
-    int20 a = create20("7700");
-    int20 b = create20("4000");
-    int20 sum = add20(a, b);
-    print20(a);
-    printf("\n");
-    print20(b);
-    printf("\n");
-    print20(sum);
-    printf("\n");
+
+    /* Aufgabe 6
+     int20 a = create20("7700");
+     int20 b = create20("4000");
+     int20 sum = add20(a, b);
+     print20(a);
+     printf("\n");
+     print20(b);
+     printf("\n");
+     print20(sum);
+     printf("\n");
+ */
+
+    /*    Aufgabe 7
+    int i;
+    int *ip;
+    printf("Eingabe: ");
+    scanf("%d", &i);
+    ip = &i;
+    printf("Eingabe war %d\n", *ip);*/
+
 
     return 0;
 };
